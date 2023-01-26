e771e1da2b46750908d7c4fbad1047687fa95185
Gabe <gbennettbrandt@gmail.com>
2023-01-26 15:07:16 -0600

    Committing bundles.js should be ignored

--- src/components/related_products/ComparisonModal.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useState, useEffect} from 'react';
-
-export default function ComparisonModal(props) {
-  return (
-    <section className='related-product'>
-      <h1>Comparison Modal Component</h1>
-    </section>
-  );
-}
--- src/components/related_products/ProductCard.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useState, useEffect} from 'react';
-
-export default function ProductCard(props) {
-  return (
-    <section className='related-product'>
-      <h1>Product Card Component</h1>
-    </section>
-  );
-}
