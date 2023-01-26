cfdc7f1345ea2a81048a8fa9d4cfb8e646876d59
Gabe <gbennettbrandt@gmail.com>
2023-01-26 08:47:33 -0600

    Changing Overview file names - also installed yarn on my instance

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const OIndex = () => {
+const Overview = () => {
 
   return (
     <h1>
@@ -9,4 +9,4 @@ const OIndex = () => {
   );
 }
 
-export default Oindex;
+export default Overview;
