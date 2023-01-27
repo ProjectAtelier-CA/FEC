8e03bd40cf9fb64aad8d9d8bd61b3156f2ec47a9
soniapotato <soniaramlall@gmail.com>
2023-01-27 11:39:06 -0500

    More thorough outline of Questions and Answers

--- src/components/qa/QuestionListItem.jsx
+++ src/components/qa/QuestionListItem.jsx
@@ -1,12 +1,15 @@
-import React, {useState, useEffect} from 'react';
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/extensions */
 
-const QuestionListItem = (props) => {
+import React, { useState, useEffect } from 'react';
+import AnswerList from './AnswerList.jsx';
 
+export default function QuestionListItem() {
   return (
-    <section id='question-list-items'>
-      In Questions List Items
+    <section style={{ border: 'Orange 2px solid' }} className="question-list-items">
+      <div> Q: the question will go here with Helpful button and Add answer</div>
+      <AnswerList />
+      <div />
     </section>
-  )
+  );
 }
-
-export default QuestionListItem;
