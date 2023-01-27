4cb74e3cd42048955fe38f0ed99199e5ece67e97
SHENGFENG WU <105718650+PHANTOMS0308@users.noreply.github.com>
2023-01-27 13:07:04 -0500

    Fixed the bugs in RelatedProduct Component (#33)
    * Add App.jsx
    
    * Add Index.js for Related Products
    
    * Partially finish the repo architecture
    
    * modify related_products/ & .gitignore
    
    * Syntax Fixed, should have no bugs now

--- src/components/related_products/RelatedProducts.jsx
+++ src/components/related_products/RelatedProducts.jsx
@@ -1,13 +1,13 @@
 // Now this is the index.jsx for related_products folder
-import React, {useState, useEffect} from 'react';
-import ProductCarousel from './ProductCarousel.jsx';
-import ProductCard from './ProductCard.jsx';
+import React from 'react';
+import ProductCarousel from './ProductCarousel';
+import ProductCard from './ProductCard';
 
 export default function RelatedProducts(props) {
   return (
-    <section className='related-product'>
+    <section className="related-product">
       <h1>Related Products Component</h1>
-      <Carousel>{ ProductCard }</Carousel>
+      <ProductCarousel>{ <ProductCard /> }</ProductCarousel>
     </section>
   );
 }
