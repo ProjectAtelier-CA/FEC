cc4e39bc2a5f1331d591a39272bbb8154b4d6c7a
Gabe <gbennettbrandt@gmail.com>
2023-01-27 10:23:25 -0600

    Passing lint for all overview files

--- src/components/overview/Overview.jsx
+++ src/components/overview/Overview.jsx
@@ -1,33 +1,34 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 
 // NAV
-import Nav from './navbar.jsx'
+import Nav from './navbar';
 
 // BANNER
-import Banner from './announcement.jsx'
+import Banner from './announcement';
 
 // MAIN IMAGE
-import ImageCarousel from './main_image/image_carousel.jsx'
-import Thumbnails from './main_image/thumbnails.jsx'
+import ImageCarousel from './main_image/image_carousel';
+import Thumbnails from './main_image/thumbnails';
 
 // PRODUCT INFO
-import ProductInfo from './product_header/product_info.jsx'
+import ProductInfo from './product_header/product_info';
 
 // STYLES
-import Styles from './styles/styles.jsx'
-import Buttons from './buttons/buttons.jsx'
+// eslint-disable-next-line import/no-named-as-default
+import Styles from './styles/styles';
+import Buttons from './buttons/buttons';
 
 // DESCRIPTION
-import Description from './product_description/product_description.jsx'
+import Description from './product_description/product_description';
 
-const Overview = () => {
-
+export default function Overview() {
   return (
     <>
-    <Nav />
-    <h1>
-      Overview Component
-    </h1>
+      <Nav />
+      <h1>
+        Overview Component
+      </h1>
       <Banner />
       <ImageCarousel />
       <Thumbnails />
@@ -38,5 +39,3 @@ const Overview = () => {
     </>
   );
 }
-
-export default Overview;
