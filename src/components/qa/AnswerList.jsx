2705198c36f8a87c5b266c9643070a907f5ae62f
SONIA RAMLALL <111000931+Soniapotato@users.noreply.github.com>
2023-01-27 18:50:45 -0500

    Forgot to Branch  (#35)
    * Initialize Framework
    
    * QA index.jsx created
    
    * QA index complete
    
    * Exported
    
    * Forgot to Branch did it in main. Created 3/4 components
    
    * Forgot changed made in App.jsx; updated to correct name
    
    * Forgot to save this file
    
    * More thorough outline of Questions and Answers
    
    * Forgot to Branch
    
    ---------
    
    Co-authored-by: Andrew Feng <96857353+bxandrew@users.noreply.github.com>

--- src/components/qa/AnswerList.jsx
+++ src/components/qa/AnswerList.jsx
@@ -1,14 +1,14 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable import/extensions */
+/* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
 
-export default function AnswerList() {
+export default function AnswerList({ answer }) {
 // map the props.answers
 // use some state to render
+
   return (
     <section>
-      <div>
-        A: Here is an answer
+      <div className="answer-body">
+        A: answers go here
       </div>
       <div> Here is user information, helpful button, report button</div>
       <div> Picture here they are clickable </div>
