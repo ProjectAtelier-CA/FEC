9f8e3ef115850890131b2e030439ee85a963f624
Andrew Feng <bxandrew@gmail.com>
2023-01-27 20:19:00 -0500

    implement More Reviews button loading functionality. Implement a rough overflow-y css structure for the scrolling feature when loading more reviews.

--- src/components/ratings_reviews/reviews_list/ReviewsCardList.jsx
+++ src/components/ratings_reviews/reviews_list/ReviewsCardList.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReviewCard from './ReviewCard';
 import ActionButtons from './ActionButtons';
 
 export default function ReviewsCardList({ productReviews }) {
+  const [reviewIndex, setReviewIndex] = useState(2); // Start it off at two reviews
+
   const reviewElements = productReviews.map((review) => (
     <ReviewCard key={review.review_id} review={review} />
   ));
 
+  const handleMoreClick = () => {
+    setReviewIndex(reviewIndex + 2);
+  };
+
   return (
     <>
       <h4>ReviewsCardList (List of Review Cards)</h4>
-      {reviewElements}
+      <div className="review-scroll">
+        {reviewElements.slice(0, reviewIndex)}
+      </div>
       <br />
-      <ActionButtons />
+      <ActionButtons
+        handleMoreClick={handleMoreClick}
+        totalReviews={productReviews.length}
+        reviewIndex={reviewIndex}
+      />
     </>
   );
 }
