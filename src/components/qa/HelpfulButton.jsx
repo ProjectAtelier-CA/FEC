363465351a00b523abe621a0bebcc72f1641de57
SONIA RAMLALL <111000931+Soniapotato@users.noreply.github.com>
2023-01-30 18:08:19 -0500

    Helpful button  (#48)
    * Initialize Framework
    
    * QA index.jsx created
    
    * QA index complete
    
    * Exported
    
    * Forgot to Branch did it in main. Created 3/4 components
    
    * Forgot changed made in App.jsx; updated to correct name
    
    * Forgot to save this file
    
    * More thorough outline of Questions and Answers
    
    * Forgot to Branch
    
    * EVERYTHING WORKS THIS IS FALL BACK
    
    * Question and Answer list items rendered out. Data passed around. Next is to get the buttons working
    
    * Fixed small changed aka deleted parenthesis
    
    * small changed commited to pull in new merges
    
    * Added buttons for show more and report. Saving progress for showmore/showless buttons working
    
    * Cleaned up all the files, and connected to the API and creating buttons and organized questionss by helpfulness
    
    ---------
    
    Co-authored-by: Andrew Feng <96857353+bxandrew@users.noreply.github.com>

---
+++ src/components/qa/HelpfulButton.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function HelpfulButton({ helpfulness, id, type }) {
+  const [clicked, setClicked] = useState(false);
+  const [help, setHelp] = useState(helpfulness);
+
+  const upVote = () => {
+    setClicked(true);
+    setHelp(help + 1);
+    console.log('tis is question_id', id);
+    axios.post('http://localhost:8081/helpful', null, { params: { id, type } });
+  };
+
+  const downVote = () => {
+    setClicked(false);
+    setHelp(help - 1);
+  };
+
+  return (
+    <>
+      <span className="helpful-button">
+        Helpful?
+        {' '}
+        |
+        {' '}
+        {!clicked ? (
+          <button className="helpful-button" onClick={upVote} type="button">
+            {' '}
+            Yes(
+            {help}
+            )
+          </button>
+        ) : null}
+      </span>
+      {clicked ? (
+        <button className="helpful-button" onClick={downVote} type="button">
+          {' '}
+          Yes(
+          {help}
+          )
+        </button>
+      ) : null}
+
+    </>
+  );
+}
