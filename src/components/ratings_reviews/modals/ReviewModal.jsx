bceca0bf981ca910d22e7c5a81bf80cce4081589
Andrew Feng <bxandrew@gmail.com>
2023-01-31 11:45:44 -0500

    uncommenting some comments out of review modal

--- src/components/ratings_reviews/modals/ReviewModal.jsx
+++ src/components/ratings_reviews/modals/ReviewModal.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import Characteristic from './Characteristic';
 import ReviewErrorMessage from './ReviewErrorMessage';
 
