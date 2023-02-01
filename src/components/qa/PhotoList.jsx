5330a2c992f0c39b9ebb406e26c97a88793a80e7
soniapotato <soniaramlall@gmail.com>
2023-01-31 19:27:40 -0500

    Got modal to pop up for image clicked on in answer

--- src/components/qa/PhotoList.jsx
+++ src/components/qa/PhotoList.jsx
@@ -1,11 +1,28 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
+import ModalImage from 'react-modal-image';
 
 export default function PhotoList({ photo }) {
+  const [open, setOpen] = useState(false);
+
+  //   return (
+  //     <span>
+  // <Modal
+  //   <img className="image-answer" alt="img-answer" onClick={setOpen} src={photo.url} />
+  //   isOpen={open}
+  //   onRequestClose={() => setOpen(false)}
+  //   style={customStyles}
+  // >
+
+  // </Modal>
+  //     </span>
+  //   )
   return (
-    <div>
-      <img className="image-answer" alt="img-answer" src={photo.url} />
-      {' '}
-    </div>
+    <ModalImage
+      className="image-answer"
+      small={photo.url}
+      medium={photo.url}
+      large={photo.url}
+    />
   );
 }
