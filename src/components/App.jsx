7df4cb7cdd174ea6dd691126ba8f9d4cee7ce1d2
Andrew Feng <bxandrew@gmail.com>
2023-02-04 15:37:40 -0500

    implement working search feature with debounce. Implementing this into main branch via pull request.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,9 +16,9 @@ export default function App() {
       {/* This component provides reference for Star Rating component, don't remove it */}
       <StarReference />
       <h1 data-testid="app-test">All Our Components</h1>
-      {/* <Overview /> */}
-      {/* <RelatedProducts id={id} setId={setId} /> */}
-      {/* <QuestionsAnswers /> */}
+      <Overview />
+      <RelatedProducts id={id} setId={setId} />
+      <QuestionsAnswers />
       <RatingsReviews productID={37331} productName={"Dummy Product Name"} />
     </>
   );
