4e663c9182b73a32dea437a068d0f5716a009edb
Gabe Bennett-Brandt <gbennettbrandt@gmail.com>
2023-02-06 15:09:28 -0600

    Adding sale detection in overview banner (#84)
    * Updating overview components folder with empty files
    
    * Adding overview.jsx
    
    * Adding overview.jsx
    
    * Deleting extra component file
    
    * Fixing Oindex.jsx
    
    * Changing Overview file names - also installed yarn on my instance
    
    * Revert "Changing Overview file names - also installed yarn on my instance"
    
    This reverts commit cfdc7f1345ea2a81048a8fa9d4cfb8e646876d59.
    
    * Cleaning up Overview
    
    * Matching file names in App.jsx
    
    * Initial component skeleton
    
    * Committing bundles.js should be ignored
    
    * Fixing react syntax issues
    
    * Delete package-lock.json
    
    * Delete bundle.js
    
    * Adding buttons
    
    * Passing lint for all overview files
    
    * TEst commit
    
    * Saving work
    
    * Saving latest style changes
    
    * Saving updates to style settings
    
    * Saving stylesheet organization
    
    * Saving before merging
    
    * Minor css tweaks
    
    * Saving changes:
    
    * Saving carousel work
    
    * Saving changes to server routes, styles, and carousel
    
    * Saving changes to carousel/css
    
    * Saving work - updates to image carousel, thumbnails, and style selectors
    
    * Saving work Tuesday
    
    * Saving progress on styles and buttons
    
    * Commiting to enable merge
    
    * Improved thumbnail scroll with styles/carousel integration.
    
    * Saving work
    
    * Saving work - style updates accross the board plus importing font
    
    * Saving progress - fixed buttons
    
    * Saving changes
    
    * Delete coverage directory
    
    * Saving work~
    
    * Refactoring thumbnails and resolving bugs
    
    * Implementing darkmode and saving work with thumbnail refactor
    
    * Fixing scroll error on product-id change
    
    * Saving work
    
    * Sharing styles via App.jsx
    
    * Conditional loading, new loading animation
    
    * Conditional loading of overview, then render remaining components
    
    * Saving work - implementing sale detector
    
    ---------
    
    Co-authored-by: Andrew Feng <96857353+bxandrew@users.noreply.github.com>
    Co-authored-by: SONIA RAMLALL <111000931+Soniapotato@users.noreply.github.com>

--- src/components/overview/announcement.jsx
+++ src/components/overview/announcement.jsx
@@ -1,14 +1,58 @@
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
 
-export default function Banner() {
+export default function Banner({
+  styles, setStyle, setIndex, setStyleObject, clickStyle,
+}) {
+  const [sales, setSales] = useState({});
+
+  useEffect(() => {
+    const indecesArray = [0];
+    styles.forEach((style) => {
+      const N = indecesArray.length;
+      indecesArray.push(indecesArray[N - 1] + style.photos.length);
+    });
+
+    const indexStyles = styles.map((style, index) => ({ style, index, imageIndex: indecesArray[index] }));
+    const saleStyles = indexStyles.filter((item) => (item.style.sale_price !== null));
+    setSales(saleStyles);
+  }, [styles]);
+
+  function handleClick(event) {
+    const id = parseInt(event.target.id, 10);
+    setStyle(id);
+    setIndex(parseInt(event.target.dataset.image, 10));
+    setStyleObject(styles[id]);
+    clickStyle(true);
+  }
+
   return (
-    <div className="banner">
-      <em>SITE-WIDE ANNOUNCEMENT MESSAGE! </em>
-      -- SALE / DISCOUNT
-      <b> OFFER</b>
-      --
-      <a href="http://www.google.com">CHECK OUT THE ITEM</a>
-    </div>
+    sales.length > 0
+      ? (
+        <div className="banner">
+          <em>&#8212;&nbsp;DON&#39;T MISS BIG SAVINGS:&nbsp;</em>
+          <div className="sales">
+            {
+              sales.map((sale) => (
+                <div>
+                  <a key={sale.style.id} className="sale" onClick={handleClick} id={sale.index} data-image={sale.imageIndex}>
+                    $
+                    {sale.style.original_price - sale.style.sale_price}
+                    {' '}
+                    off
+                  </a>
+                  &nbsp;
+                </div>
+              ))
+            }
+            &nbsp;&#8212;
+          </div>
+        </div>
+      )
+      : null
   );
 }
