92759bced3315572baf66ae4c77d4ace29f009c0
Gabe Bennett-Brandt <gbennettbrandt@gmail.com>
2023-02-06 13:27:25 -0600

    Conditional rendering of components after overview (#83)
    * Updating overview components folder with empty files
    
    * Adding overview.jsx
    
    * Adding overview.jsx
    
    * Deleting extra component file
    
    * Fixing Oindex.jsx
    
    * Changing Overview file names - also installed yarn on my instance
    
    * Revert "Changing Overview file names - also installed yarn on my instance"
    
    This reverts commit cfdc7f1345ea2a81048a8fa9d4cfb8e646876d59.
    
    * Cleaning up Overview
    
    * Matching file names in App.jsx
    
    * Initial component skeleton
    
    * Committing bundles.js should be ignored
    
    * Fixing react syntax issues
    
    * Delete package-lock.json
    
    * Delete bundle.js
    
    * Adding buttons
    
    * Passing lint for all overview files
    
    * TEst commit
    
    * Saving work
    
    * Saving latest style changes
    
    * Saving updates to style settings
    
    * Saving stylesheet organization
    
    * Saving before merging
    
    * Minor css tweaks
    
    * Saving changes:
    
    * Saving carousel work
    
    * Saving changes to server routes, styles, and carousel
    
    * Saving changes to carousel/css
    
    * Saving work - updates to image carousel, thumbnails, and style selectors
    
    * Saving work Tuesday
    
    * Saving progress on styles and buttons
    
    * Commiting to enable merge
    
    * Improved thumbnail scroll with styles/carousel integration.
    
    * Saving work
    
    * Saving work - style updates accross the board plus importing font
    
    * Saving progress - fixed buttons
    
    * Saving changes
    
    * Delete coverage directory
    
    * Saving work~
    
    * Refactoring thumbnails and resolving bugs
    
    * Implementing darkmode and saving work with thumbnail refactor
    
    * Fixing scroll error on product-id change
    
    * Saving work
    
    * Sharing styles via App.jsx
    
    * Conditional loading, new loading animation
    
    * Conditional loading of overview, then render remaining components
    
    ---------
    
    Co-authored-by: Andrew Feng <96857353+bxandrew@users.noreply.github.com>
    Co-authored-by: SONIA RAMLALL <111000931+Soniapotato@users.noreply.github.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import QuestionsAnswers from './qa/QuestionsAnswers';
 import Overview from './overview/Overview';
 import StarReference from './shared/StarReference';
 import '../styles/styles.scss';
+import { GiAmericanShield, GiOctopus } from 'react-icons/gi';
 
 /* ----------- Set up id state -------------- */
 export default function App() {
@@ -15,15 +16,14 @@ export default function App() {
   const [appAvgRating, setAppAvgRating] = useState(5);
   const [productDetails, setDetails] = useState({});
   const [styles, setStyles] = useState([]);
+  const [overviewLoading, setOverviewLoading] = useState(true);
   // console.log(appAvgRating);
 
   return (
     <>
       {/* This component provides reference for Star Rating component, don't remove it */}
       <StarReference />
-      {/* <h1 data-testid="app-test">All Our Components</h1> */}
       <Overview
-        product_id={id}
         goDark={goDark}
         dark={dark}
         appAvgRating={appAvgRating}
@@ -31,11 +31,24 @@ export default function App() {
         setDetails={setDetails}
         styles={styles}
         setStyles={setStyles}
+        isLoading={overviewLoading}
+        setLoading={setOverviewLoading}
+        product_id={id}
       />
-      <RelatedProducts id={id} setId={setId} />
-      <h3 className="testing-header"> Questions and Answers</h3>
-      <QuestionsAnswers id={id} productName={"A Purty Pink Jacket"}/>
-      <RatingsReviews productID={id} productName={"Dummy Product Name"} setAppAvgRating={setAppAvgRating} />
+      {
+        overviewLoading
+          ? (
+            <GiAmericanShield className="overview-loader" />
+          )
+          : (
+            <>
+              <RelatedProducts id={id} setId={setId} />
+              <h3 className="testing-header"> Questions and Answers</h3>
+              <QuestionsAnswers id={id} productName="A Purty Pink Jacket" />
+              <RatingsReviews productID={id} productName="Dummy Product Name" setAppAvgRating={setAppAvgRating} />
+            </>
+          )
+      }
     </>
   );
 }
