ffcda1b23d9f5a09d5a76534143281be890698ec
Gabe <gbennettbrandt@gmail.com>
2023-02-06 18:35:05 -0600

    COmpleting merge

--- src/components/overview/navbar.jsx
+++ src/components/overview/navbar.jsx
@@ -19,7 +19,6 @@ export default function Nav({ goDark, dark }) {
 
   function handleSearch(event) {
     console.log(event.target.value);
-    // setSearch(event.target.value);
   }
 
   function setDarkMode() {
