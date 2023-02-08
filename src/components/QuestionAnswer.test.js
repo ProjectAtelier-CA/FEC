bffb41ae37441cb2cae236fa119b91c85e3ec85e
SONIA RAMLALL <111000931+Soniapotato@users.noreply.github.com>
2023-02-07 20:14:53 -0500

    Testing at 60 percent  (#93)
    * Initialize Framework
    
    * QA index.jsx created
    
    * QA index complete
    
    * Exported
    
    * Forgot to Branch did it in main. Created 3/4 components
    
    * Forgot changed made in App.jsx; updated to correct name
    
    * Forgot to save this file
    
    * More thorough outline of Questions and Answers
    
    * Forgot to Branch
    
    * EVERYTHING WORKS THIS IS FALL BACK
    
    * Question and Answer list items rendered out. Data passed around. Next is to get the buttons working
    
    * Fixed small changed aka deleted parenthesis
    
    * small changed commited to pull in new merges
    
    * Added buttons for show more and report. Saving progress for showmore/showless buttons working
    
    * Cleaned up all the files, and connected to the API and creating buttons and organized questionss by helpfulness
    
    * Forgot to save this file when committing
    
    * Updated date format thanks to Andrew
    
    * Added sorting to answer to show by helpfulness and created some css styling
    
    * Added answer modal. Sorted answers and added button to show more answers and less answers not style:
    
    * Delete package-lock.json
    
    * Got modal to pop up for image clicked on in answer
    
    * Have modals for questions, not authenticated or connecting to server yet
    
    * Added question Modal and updated answer modal
    
    * Got the question post to talk to server and cleaned up console.logs everywhere
    
    * First Testing cases
    
    * Search Bar Works!
    
    * Finished up report button with server connection as well as try to upload image for add answer response no luck
    
    * Saving changes made to files
    
    * Worked on getting rid of extra button and finesse the layout
    
    * test
    
    * Git hub merge test
    
    * Saving Files
    
    * Fixing changes
    
    * Trickling data from app to my component
    
    * Finished up styling and functionalties. Sort by seller working and all the buttons are working More styling is required for the modals
    
    * connecting dynamically to render based on related products clicked. Eliminated style conflicts with overview
    
    * Satisfied with functionality and css styling. Will make changes later if necessary
    
    * Removed toggling from helpful yes button
    
    * Updated the color to match overview
    
    * Dark Mode activated
    
    * Made small css changes to  color and hover handling
    
    * matched the colors
    
    * Implemented tests at 60 percent coverage
    
    ---------
    
    Co-authored-by: Andrew Feng <96857353+bxandrew@users.noreply.github.com>

--- src/components/QuestionAnswer.test.js
+++ src/components/QuestionAnswer.test.js
@@ -4,6 +4,14 @@ import userEvent from '@testing-library/user-event';
 import { render, screen, waitFor } from '@testing-library/react';
 import HelpfulButton from './qa/HelpfulButton';
 import ReportButton from './qa/ReportButton';
+import QuestionList from './qa/QuestionList';
+import QuestionsAnswers from './qa/QuestionsAnswers';
+import AnswerList from './qa/AnswerList';
+import AnswerListItem from './qa/AnswerListItem';
+import QuestionListItem from './qa/QuestionListItem';
+import AddAnswer from './qa/AddAnswer';
+import AddQuestion from './qa/AddQuestion';
+import PhotoList from './qa/PhotoList';
 
 describe('Jest', () => {
   const user = userEvent.setup();
@@ -21,5 +29,145 @@ describe('Jest', () => {
   it('should have  a report button', () => {
     render(<ReportButton />);
     expect(screen.getByTestId('report-button')).toBeInTheDocument();
+  });
+
+  it('should render main page', () => {
+    render(<QuestionsAnswers id={37316} productName="testy" />);
+    return waitFor(() => expect(screen.queryByText(/Loading/)).not.toBeInTheDocument());
+  });
+
+  it('should render main page', () => {
+    render(<PhotoList photo={[]} />);
+    return waitFor(() => expect(screen.queryByText(/Loading/)).not.toBeInTheDocument());
+  });
+
+
+
+  it('should render question list item', () => {
+    render(<QuestionListItem
+      q={{
+        question_id: 644533,
+        question_body: 'Here is a question, what time is it?',
+        question_date: '2022-12-15T00:00:00.000Z',
+        asker_name: 'Edgar',
+        question_helpfulness: 26,
+        reported: false,
+        answers: {
+          5989836: {
+            id: 5989836,
+            body: 'test to add an answer',
+            date: '2022-12-15T00:00:00.000Z',
+            answerer_name: 'tester',
+            helpfulness: 5,
+            photos: [],
+          },
+        },
+      }}
+      productName="testy"
+    />);
+    return waitFor(() => expect(screen.queryByText(/Loading/)).not.toBeInTheDocument());
+  });
+
+  it('should render answer list ', () => {
+    render(<AnswerList answers={[{
+      id: 5989848,
+      body: 'testing answe body',
+      date: '2022-12-15T00:00:00.000Z',
+      answerer_name: 'testuser',
+      helpfulness: 0,
+      photos: [],
+    }]}
+    />);
+    return waitFor(() => expect(screen.queryByText(/Loading/)).not.toBeInTheDocument());
+  });
+
+  it('should render answer list item ', () => {
+    render(<AnswerListItem ans={{
+      id: 5989848,
+      body: 'testing answe body',
+      date: '2022-12-15T00:00:00.000Z',
+      answerer_name: 'testuser',
+      helpfulness: 0,
+      photos: [],
+    }}
+    />);
+    return waitFor(() => expect(screen.queryByText(/Loading/)).not.toBeInTheDocument());
+  });
+  it('should render add answer', () => {
+    render(<AddAnswer />);
+    expect(screen.getByRole('button', { name: /Add Answer/ })).toBeInTheDocument();
+  });
+
+  it('should render add answer', () => {
+    render(<AddQuestion />);
+    expect(screen.getByRole('button', {name: /Add Question/ })).toBeInTheDocument();
+  });
+  it('should render add answer', () => {
+    render(<AddQuestion />);
+    expect(screen.queryByText(/Add Questio/)).toBeInTheDocument();
+  });
+
+  it('should render questionlist', () => {
+    render(<QuestionList
+      productIdData={[{
+        product_id: '37311',
+        results: [
+          {
+            question_id: 644533,
+            question_body: 'Here is a question, what time is it?',
+            question_date: '2022-12-15T00:00:00.000Z',
+            asker_name: 'Edgar',
+            question_helpfulness: 26,
+            reported: false,
+            answers: {
+              5989836: {
+                id: 5989836,
+                body: 'test to add an answer',
+                date: '2022-12-15T00:00:00.000Z',
+                answerer_name: 'tester',
+                helpfulness: 5,
+                photos: [],
+              },
+              5989848: {
+                id: 5989848,
+                body: 'testing answe body',
+                date: '2022-12-15T00:00:00.000Z',
+                answerer_name: 'testuser',
+                helpfulness: 0,
+                photos: [],
+              },
+              5989855: {
+                id: 5989855,
+                body: 'It is time to have awesome day and look dope in your camo onesie',
+                date: '2022-12-16T00:00:00.000Z',
+                answerer_name: 'rando',
+                helpfulness: 0,
+                photos: [],
+              },
+              5989887: {
+                id: 5989887,
+                body: "it's 1:52",
+                date: '2022-12-16T00:00:00.000Z',
+                answerer_name: 'hello',
+                helpfulness: 0,
+                photos: [],
+              },
+              5989893: {
+                id: 5989893,
+                body: 'This is an answer to your question!',
+                date: '2022-12-16T00:00:00.000Z',
+                answerer_name: 'orchid',
+                helpfulness: 2,
+                photos: [],
+              },
+            },
+          },
+        ],
+      },
+      ]}
+      productName="testy"
+      product_id={37311}
+    />);
+    return waitFor(() => expect(screen.queryByText(/Loading/)).not.toBeInTheDocument());
   });
 });
